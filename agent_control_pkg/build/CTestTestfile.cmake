0cd5bfcf089f55961c3b6e0017c4f39a542b55cd
Atakan <yamana19@itu.edu.tr>
2025-10-07 21:31:09 +0300

    chore: ignore build artifacts and keep standalone tester

--- agent_control_pkg/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ataka/OneDrive/Masaüstü/docs/multi-agent-formation-control-under-disturbances/agent_control_pkg
-# Build directory: C:/Users/ataka/OneDrive/Masaüstü/docs/multi-agent-formation-control-under-disturbances/agent_control_pkg/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("_deps/yaml-cpp-build")
